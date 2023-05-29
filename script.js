16c078ef34548354cd36a19301d99b1f636120d5
German Novikov <difenz1k@gmail.com>
2023-05-29 15:37:32 +0300

    ...

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const items = [
   {name: 'xylinet', img: 'IMG/case/caliber.png', chance: 3},
   {name: 'Pika', img: 'IMG/case/MA.png', chance: 4},
   {name: '1500ballov', img: 'IMG/case/bohkazip.png', chance: 5},
-  {name: '1500ballov', img: 'IMG/case/boshkijar.png', chance: 8},
+  {name: '1500ballov', img: 'IMG/case/boshkijar.png', chance: 6},
   {name: 'achonet', img: 'IMG/case/achonet.png', chance: 10},
   {name: '100ballov', img: 'IMG/case/55percent.png', chance: 25},
   {name: '50ballov', img: 'IMG/case/prize.png', chance: 28}
